6a307b98f934af8c2758152d0f885b543dac8b00
THARAK <104959933+VU21CSEN0100414THARAK@users.noreply.github.com>
2023-12-25 19:42:44 +0530

    Update Hardcoding.py

--- Hardcoding.py
+++ Hardcoding.py
@@ -12,12 +12,12 @@ def main():
       # Lists to store day and corresponding temperature values
       days = list(range(1, num_days + 1))
       temperatures = [quadratic_temperature_model(day, a, b, c) for day in days]
-      # Plotting
+      # Plotting 
       plt.plot(days, temperatures, label=f'Temperature: {a}x^2 + {b}x + {c}')
       plt.title('Weather Modeling')
       plt.xlabel('Day')
       plt.ylabel('Temperature (°C)')
       plt.legend()
       plt.show()
-
+      #This line is for modiyfing or adding comments to the code 
 main()
